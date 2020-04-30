2fe9877c99bb0dd68f5365ab983624ebb4d0f12b
rithikaprakash <60447231+rithikaprakash@users.noreply.github.com>
2020-05-01 04:39:29 +0530

    Remove character

---
+++ src/com/tgt/igniteplus/Remove_char.java
@@ -0,0 +1,14 @@
+package com.tgt.igniteplus;
+
+import java.util.Scanner;
+
+public class Remove_char {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string :");
+        String str = sc.nextLine();
+        str = str.toLowerCase();
+        str = str.replaceAll("t", "");
+        System.out.println("The modified String is : " + str);
+    }
+}
