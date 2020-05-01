69787f4487d78b4cd124170d67adac60a663bd48
rithikaprakash <60447231+rithikaprakash@users.noreply.github.com>
2020-05-01 06:02:53 +0530

    Modified-StringDigits

--- src/com/tgt/igniteplus/StringDigits.java
+++ src/com/tgt/igniteplus/StringDigits.java
@@ -1,4 +1,21 @@
+//Review comment : why do you need flag ? use System.out.println() in if-else block itself.
+
+//If a string contains only digits
 package com.tgt.igniteplus;
 
+import java.util.Scanner;
+
 public class StringDigits {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string :");
+        String str = sc.nextLine();
+        if(str.matches("[0-9]+")) //regex function
+             System.out.println("String contains only digits ");
+        else
+            {
+                System.out.println("String does not contain only digits ");
+            }
+
+        }
+    }
